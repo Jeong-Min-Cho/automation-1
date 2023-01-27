61b4250ab3251f7b019ea673a6155af160d12162
Brutalation <brutalation@gmail.com>
2023-01-27 13:00:25 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature4Page.java
+++ src/main/java/pageobject/feature4Page.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 
 import framework.AADriver;
 
-import org.openqa.selenium.WebElement;
-import java.lang.reflect.Field;
 //<IMPORTS>
 
 public class feature4Page {
