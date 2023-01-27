c04b151bce7cb6a5baa9dfa39ba3481e39633b3b
Brutalation <brutalation@gmail.com>
2023-01-27 13:24:58 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature4Page.java
+++ src/main/java/pageobject/feature4Page.java
@@ -19,6 +19,8 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
+public static By GoogleSearchBoxText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
+
 //<XPATHS>
 
 /*Page Constructor*/
