39e42114f5593c654ad41483f7e063fe66cf13aa
Brutalation <brutalation@gmail.com>
2023-01-27 12:42:22 -0500

    Commtting File: PageObjectFile

---
+++ src/main/java/pageobject/feature4Page.java
@@ -0,0 +1,38 @@
+package pageobject;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import framework.AADriver;
+
+import org.openqa.selenium.WebElement;
+import java.lang.reflect.Field;
+//<IMPORTS>
+
+public class feature4Page {
+
+public static WebDriver driver;
+AADriver myDriver;
+public static Properties properties;
+
+
+/*Xpaths*/
+public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
+public static By searchBox = By.xpath("//input[@title='Search']");
+//<XPATHS>
+
+/*Page Constructor*/
+public feature4Page(){
+
+}
+
+
+public feature4Page(WebDriver driver){
+this.driver= driver;
+myDriver = new AADriver(this.driver);
+}
+
+//<METHOD>
+}
