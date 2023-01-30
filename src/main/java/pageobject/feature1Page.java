b78327f426d09a2fc94aa65d8c71c729960b41fc
Brutalation <brutalation@gmail.com>
2023-01-30 10:51:30 -0500

    Commtting File: PageObjectFile

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.WebDriver;
 
 import framework.AADriver;
 
-import java.lang.reflect.Field;
-import org.openqa.selenium.WebElement;
-import static org.junit.Assert.assertEquals;
 //<IMPORTS>
 
 public class feature1Page {
@@ -22,9 +19,7 @@ public static Properties properties;
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
 public static By searchBox = By.xpath("//input[@title='Search']");
-public static By defaultText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
-
-public static By defaultButton = By.xpath("(//input[contains(@class,'gNO89b')])[2]");
+public static By GoogleSearchBoxText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
 
 //<XPATHS>
 
